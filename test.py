f96ca46105ce66d643cc5439723b0ff76ed13455
ashwinpra <ashprasanth2003@gmail.com>
2023-07-13 23:40:14 +0530

    remove unnecessary file

--- test.py
+++
@@ -1,7 +0,0 @@
-from tabulate import tabulate
-import pandas as pd
-
-df = pd.read_csv('available_electives.csv')
-
-print(tabulate(df, headers='keys', tablefmt='psql'))
-print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
